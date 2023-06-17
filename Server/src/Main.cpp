83377ed627e66098b734a8948e638e0e36adf083
Jonathon Macleod <no-reply@jonathonmacleod.co.uk>
2023-06-17 23:34:57 +0100

    Added walls and floor to surround cube

--- Server/src/Main.cpp
+++ Server/src/Main.cpp
@@ -63,6 +63,9 @@ int main(int argc, char* args[]) {
 	}
 	spdlog::info("Using GLEW {}", (char*) glewGetString(GLEW_VERSION));
 
+	// Set OpenGL renderings states to prevent items from rendering when behind other items
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CCW);
 	glCullFace(GL_BACK);
@@ -79,20 +82,56 @@ int main(int argc, char* args[]) {
 																			   GetColour("70A288"), GetColour("DAB785"),
 																			   GetColour("D5896F"), GetColour("FFA0FD"));
 
+	// Create surface models
+	GSA::Graphics::Model* surfaceModel = GSA::Graphics::Model::CreateColouredCube(GetColour("304D6D"), GetColour("304D6D"), 
+																				  GetColour("A7CCED"), GetColour("A7CCED"), 
+																				  GetColour("63ADF2"), GetColour("63ADF2"));
+
+	// Create model transform matrices
+	glm::mat4 cubeModelMatrix = glm::mat4(1);
+	cubeModelMatrix = glm::scale(cubeModelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+	glm::mat4 leftWallModelMatrix = glm::mat4(1);
+	leftWallModelMatrix = glm::translate(leftWallModelMatrix, glm::vec3(2.125f, 0.0f, 0));
+	leftWallModelMatrix = glm::scale(leftWallModelMatrix, glm::vec3(0.25f, 3.5f, 3.0f));
+	glm::mat4 rightWallModelMatrix = glm::mat4(1);
+	rightWallModelMatrix = glm::translate(rightWallModelMatrix, glm::vec3(-2.125f, 0.0f, 0));
+	rightWallModelMatrix = glm::scale(rightWallModelMatrix, glm::vec3(0.25f, 3.5f, 3.0f));
+	glm::mat4 backWallModelMatrix = glm::mat4(1);
+	backWallModelMatrix = glm::translate(backWallModelMatrix, glm::vec3(0.0f, 0.0f, 1.625f));
+	backWallModelMatrix = glm::scale(backWallModelMatrix, glm::vec3(4.0f, 3.5f, 0.25f));
+	glm::mat4 bottomFloorModelMatrix = glm::mat4(1);
+	bottomFloorModelMatrix = glm::translate(bottomFloorModelMatrix, glm::vec3(0.0f, -1.625f, 0.0f));
+	bottomFloorModelMatrix = glm::scale(bottomFloorModelMatrix, glm::vec3(4.0f, 0.25f, 3.0f));
+
 	// Create view and perspective matrices
-	glm::mat4 viewMatrix = glm::lookAt(glm::vec3(-1, 1, -1), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-	glm::mat4 modelMatrix = glm::mat4(1);
+	glm::mat4 viewMatrix = glm::lookAt(glm::vec3(0.0f, 0.0f, -4.0f), glm::vec3(0.0f, 0.0f, -3.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	glm::mat4 perspectiveMatrix = glm::perspectiveFov(16.0f / 9.0f, 640.f, 480.f, 0.01f, 100.0f);
 
 	// Keep a window alive until closed and draw some contents to the screen
 	while(!glfwWindowShouldClose(window)) {
+		// Clear the graphics context of any previously drawn graphics
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		// Draw the triangle!
 		if(shader != NULL) {
 			shader->Bind();
 			shader->SetUniformMatrix4(0, viewMatrix);
-			shader->SetUniformMatrix4(1, modelMatrix);
 			shader->SetUniformMatrix4(2, perspectiveMatrix);
+
+			// Draw the cube
+			shader->SetUniformMatrix4(1, cubeModelMatrix);
 			cubeModel->Draw();
+
+			// Draw the surfaces
+			shader->SetUniformMatrix4(1, leftWallModelMatrix);
+			surfaceModel->Draw();
+			shader->SetUniformMatrix4(1, rightWallModelMatrix);
+			surfaceModel->Draw();
+			shader->SetUniformMatrix4(1, backWallModelMatrix);
+			surfaceModel->Draw();
+			shader->SetUniformMatrix4(1, bottomFloorModelMatrix);
+			surfaceModel->Draw();
+
 			shader->Unbind();
 		}
 
